a8f350c5654fe54fcdf9d99716fcc5d339ec5182
이나령 <thisisme20@daum.net>
2023-10-08 12:21:50 +0900

    feat: react-router-dom 세팅

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,32 @@
+import { Outlet, Route, Routes } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Navbar from "./layout/Navbar";
+import Footer from "./layout/Footer";
+
+function Layout() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  )
+}
+
 function App() {
 
   return (
-    <>
-      <div>Hello</div>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Route>
+    </Routes>
   );
 }
 
