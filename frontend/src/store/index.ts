07d4d34729ec59e61247b425469183f8be1fe352
이나령 <thisisme20@daum.net>
2023-10-09 16:45:48 +0900

    feat: redux-persist 적용

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,8 +1,28 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
+import storage from "redux-persist/lib/storage";
+import { FLUSH, PAUSE, PERSIST, REGISTER, REHYDRATE, persistReducer, persistStore } from "redux-persist";
+
+const rootReducer = combineReducers({
+  user: userReducer
+})
+
+// root라는 이름으로 local storage에 저장
+const persistConfig = {
+  key: "root",
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer
-  }
-})
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
