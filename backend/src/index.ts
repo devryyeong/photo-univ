2a96d1433b380295cdca712627f852c33d71dd17
이나령 <thisisme20@daum.net>
2023-10-10 00:26:24 +0900

    [backend] mongoose를 사용해 mongoDB 연결

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,25 @@
 import express, { Express, Request, Response } from "express";
 
+const mongoose = require("mongoose");
 const path = require("path");
 const app: Express = express();
 const port = 4000;
 const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 app.use(cors());
 app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("연결완료");
+  })
+  .catch((err) => {
+    console.log("ERROR: ", err);
+  });
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Typescript + Node.js + Express Server");
