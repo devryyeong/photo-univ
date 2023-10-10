fac912af7be6436dc1448444242f213e2645f1c6
이나령 <thisisme20@daum.net>
2023-10-10 17:17:41 +0900

    [frontend] feat: react-hook-form을 사용해 회원가입 폼의 유효성 검사 로직 추가

--- frontend/src/pages/Register/index.tsx
+++ frontend/src/pages/Register/index.tsx
@@ -1,15 +1,70 @@
 import React from 'react'
+import { SubmitHandler, useForm } from "react-hook-form";
+
+interface RegisterInputProps {
+  email: string;
+  password: string;
+  name: string;
+}
 
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+    reset
+  } = useForm<RegisterInputProps>();
+
+  const onSubmit: SubmitHandler<RegisterInputProps> = ({ email, password, name }: RegisterInputProps) => {
+    console.log("email", email);
+    reset();
+  };
+
+  const userName = {
+    required: "이름은 필수 입력 항목입니다."
+  };
+  
+  const userEmail = {
+    required: "이메일은 필수 입력 항목입니다.",
+  };
+
+  const userPassword = {
+    required: "비밀번호는 필수 입력 항목입니다.",
+    minLength: {
+      value: 4,
+      message: "비밀번호는 4자 이상입니다."
+    }
+  };
+  
   return (
     <section className="flex flex-col justify-center mt-20 max-w-[400px] m-auto">
       <div className="p-6 bg-white rounded-md shadow-md">
         <h1 className="text-3xl font-semibold text-center">회원가입</h1>
-        <form className="mt-6">
+        <form className="mt-6" onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-2">
             <label
               className="text-sm font-semibold text-gray-800"
               htmlFor="email"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="w-full px-4 py-2 mt-2 bg-white border rounded-md"
+              {...register("name", userName)}
+            />
+            {errors?.name && (
+              <div>
+                <span className="text-red-500">{errors.name.message}</span>
+              </div>
+            )}
+          </div>
+          <div className="mb-2">
+            <label
+              className="text-sm font-semibold text-gray-800"
+              htmlFor="name"
             >
               Email
             </label>
@@ -17,7 +72,32 @@ const Register = () => {
               type="email"
               id="email"
               className="w-full px-4 py-2 mt-2 bg-white border rounded-md"
+              {...register("email", userEmail)}
             />
+            {errors?.email && (
+              <div>
+                <span className="text-red-500">{errors.email.message}</span>
+              </div>
+            )}
+          </div>
+          <div className="mb-2">
+            <label
+              className="text-sm font-semibold text-gray-800"
+              htmlFor="email"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="w-full px-4 py-2 mt-2 bg-white border rounded-md"
+              {...register("password", userPassword)}
+            />
+            {errors?.password && (
+              <div>
+                <span className="text-red-500">{errors.password.message}</span>
+              </div>
+            )}
           </div>
 
           <div className="mt-6">
@@ -28,8 +108,11 @@ const Register = () => {
               회원가입
             </button>
           </div>
-          <p className="mt-8 font-light text-xs text-center text-gray-700">이미 계정이 있나요?{" "}
-            <a href="/login" className="font-medium hover:underline">로그인</a>
+          <p className="mt-8 font-light text-xs text-center text-gray-700">
+            이미 계정이 있나요?{" "}
+            <a href="/login" className="font-medium hover:underline">
+              로그인
+            </a>
           </p>
         </form>
       </div>
