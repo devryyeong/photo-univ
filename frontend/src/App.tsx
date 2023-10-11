e2a123fbb56ffec0d022e3b5b422f7f4dba1157a
이나령 <thisisme20@daum.net>
2023-10-11 22:27:05 +0900

    [frontend] feat: 에러 발생시 toast pop-up 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,21 +4,29 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Navbar from "./layout/Navbar";
 import Footer from "./layout/Footer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/ReactToastify.css"
 
 function Layout() {
   return (
     <div className="flex flex-col h-screen justify-between">
+      <ToastContainer
+        position="top-right"
+        theme="light"
+        pauseOnHover
+        autoClose={2000}
+        draggablePercent={60}
+      />
       <Navbar />
       <main className="mb-auto w-10/12 max-w-4xl mx-auto">
         <Outlet />
       </main>
       <Footer />
     </div>
-  )
+  );
 }
 
 function App() {
-
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
